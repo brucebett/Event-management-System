c395be74989df007874a865f9097c12dcdbb739b
brucebett <bett6448@gmail.com>
2024-09-17 21:39:38 +0300

    tue commit

--- Events/views.py
+++ Events/views.py
@@ -5,23 +5,26 @@ from Events.models import Events
 
 # Create your views here.
 def home(request):
+    data = Events.objects.all()
+    context = {'data': data}
     return render(request, 'home.html')
 
 def eventedit(request):
     return render(request, 'event_edit.html')
 
 def eventlist(request):
+
     return render(request, 'event_list.html')
 
 def save(request):
     if request.method == 'POST':
-        name = request.POST.get(name)
-        description = request.POST.get(description)
-        date = request.POST.get(date)
-        time = request.POST.get(time)
-        location = request.POST.get(location)
-        attendes = request.POST.get(attendes)
-        form = Events(name=name,description=description,date=date,time=time,location=location,attendes=attendes)
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        date = request.POST.get('date')
+        time = request.POST.get('time')
+        location = request.POST.get('location')
+        attendees = request.POST.get('attendees')
+        form = Events(name=name,description=description,date=date,time=time,location=location,attendees=attendees)
         form.save()
         return redirect("/")
     return render(request, 'event_list.html')
